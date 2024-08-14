599695de1fc23d1d88c2da7ba1640d96895b10b4
ConnerKT <connerthompson121@gmail.com>
2024-08-14 16:04:03 -0500

    added for deploy test

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,16 +1,13 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
 import './App.css'
 
 function App() {
-  const [count, setCount] = useState(0)
 
   return (
     <>
      <h1>Welcome to my manga frontend!</h1>
      <p>What is this?</p>
      <p>This is an application to view manga (Like MAL) </p>
+     <p>By: Conner Thompson</p>
     </>
   )
 }
